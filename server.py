ad39c0977d2423a05b7571bc97044c97aa59caaa
seandwsy <seandavidsy@gmail.com>
2024-03-18 19:05:55 -0400

    moved stylesheet to base template

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder='pages')
 
 @app.route('/')
 def index():
-    return render_template('auth.html')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
