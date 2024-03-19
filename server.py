a2d4d29cf9310f8de6692af5b7eba7f232389f13
seandwsy <seandavidsy@gmail.com>
2024-03-18 20:25:40 -0400

    finished total-recalls.html, base-logged.html, slightly altered login.html to properly route to CPSC logo

--- server.py
+++ server.py
@@ -6,5 +6,9 @@ app = Flask(__name__, template_folder='pages')
 def index():
     return render_template('login.html')
 
+@app.route('/recalls')
+def recalls():
+    return render_template('total-recalls.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
