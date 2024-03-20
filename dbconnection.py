1e0a3525bbffecbbe6f9e7e6ecb08294b4e6eb20
Akhil Kamalesh <akhilkamalesh@Akhils-MacBook-Pro.local>
2024-03-20 10:36:12 -0400

    Updated so that datetime is datetime

--- dbconnection.py
+++ dbconnection.py
@@ -2,6 +2,10 @@ import pyodbc
 import urllib.request
 import json
 import re
+from datetime import datetime
+
+
+#DO NOT RUN ANYMORE, DATA IS WITHIN DB
 
 cnxn = pyodbc.connect('Driver={ODBC Driver 18 for SQL Server};Server=tcp:cpsc-server0313.database.windows.net,1433;Database=CPSCDatabase0313;Uid=cpscadmin;Pwd=CP$CD@taB!t4454;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
 cursor = cnxn.cursor()
@@ -26,7 +30,7 @@ with urllib.request.urlopen("https://www.saferproducts.gov/RestWebServices/Recal
 
         cursor.execute("INSERT INTO [dbo].[Recalls] (RecallID, RecallDate, RecallNumberCPSC, RecallURL, Description, ProductName, \
                 ProductType, ManufacturerName, RecallReason, UnitsInCirculation, Title, IsPrioritized) VALUES (\
-                ? , ? , ? , ? , ?, ?, ?, ?, ?, ?, ?, ?)", int(data[i]['RecallID']) , data[i]['RecallDate'] , re.sub(r'\D', '', data[i]['RecallNumber'].replace(',', '.')) , data[i]['URL'].replace(',', '.') , data[i]['Description'].replace(',', '.')[0:128], \
+                ? , ? , ? , ? , ?, ?, ?, ?, ?, ?, ?, ?)", int(data[i]['RecallID']) , datetime.strptime(data[i]['RecallDate'].replace(',', '.'), '%Y-%m-%dT%H:%M:%S') , re.sub(r'\D', '', data[i]['RecallNumber'].replace(',', '.')) , data[i]['URL'].replace(',', '.') , data[i]['Description'].replace(',', '.')[0:128], \
             products[0]['Name'].replace(',', '.')[0:128] or 'n/a', products[0]['Type'].replace(',', '.')[0:128] or 'n/a', manufacturers[0]['Name'].replace(',', '.')[0:128], hazard[0]['Name'].replace(',', '.')[0:128], re.sub(r'\D', '', products[0]['NumberOfUnits'].replace(',', '')[5:11]) or 0, \
             data[i]['Title'].replace(',', '.')[0:128], False)
         
